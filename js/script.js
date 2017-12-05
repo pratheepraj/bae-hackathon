c3f9f1a102d61be1022fda0db64304d6aaf7d0fa
Pratheep Raj <pratheep.my@gmail.com>
2017-12-05 14:15:00 +0800

    Added datepicker

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('#start-date').fdatepicker({
+    initialDate: '01-01-2000',
+    format: 'mm-dd-yyyy',
+    disableDblClickSelection: true,
+    leftArrow: '<<',
+    rightArrow: '>>',
+    closeIcon: 'X',
+    closeButton: true,
+  });
+
+  $('#end-date').fdatepicker({
+    initialDate: '01-01-2017',
+    format: 'mm-dd-yyyy',
+    disableDblClickSelection: true,
+    leftArrow: '<<',
+    rightArrow: '>>',
+    closeIcon: 'X',
+    closeButton: true,
+  });
+});
